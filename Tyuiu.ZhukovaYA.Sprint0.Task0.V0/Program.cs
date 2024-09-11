eac09eef494c6170627b3b79d5b41b0da95d06e2
Yana <yana_zhukova2004@mail.ru>
2024-09-11 15:08:33 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.ZhukovaYA.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.ZhukovaYA.Sprint0.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
